cfb48bb30a5289734509f4b38ed9ce5756aba15f
Grusie <rbtjr02@naver.com>
2023-01-27 19:32:54 +0900

    mvp pattern

--- app/src/main/java/com/grusie/mvvmpractice/MainActivity.kt
+++ app/src/main/java/com/grusie/mvvmpractice/MainActivity.kt
@@ -7,9 +7,11 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.grusie.mvvmpractice.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),ViewInterface {
     lateinit var binding : ActivityMainBinding
     var model = Model()
+    var presenter = Presenter(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
@@ -17,11 +19,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clickNum(i: Int){
-        Toast.makeText(this, "$i 버튼이 클릭됨", Toast.LENGTH_SHORT).show()
+        presenter.clickNum(i)
+    }
 
-        model.inputPassword(i)
-        if(model.password.size == 4 && model.checkPassword()){
-            binding.messageSuccess.visibility = View.VISIBLE
-        }
+    override fun ToastMessage(i: Int) {
+        Toast.makeText(this, "$i 버튼이 클릭됨", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun checkPasswordMessage() {
+        binding.messageSuccess.visibility = View.VISIBLE
     }
 }
---
+++ app/src/main/java/com/grusie/mvvmpractice/ViewInterface.kt
@@ -0,0 +1,6 @@
+package com.grusie.mvvmpractice
+
+interface ViewInterface {
+    fun ToastMessage(i:Int)
+    fun checkPasswordMessage()
+}
